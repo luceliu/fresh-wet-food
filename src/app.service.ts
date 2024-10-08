a58c34fe236f6f34160bdf73329285233f457ac5
Luce <lucy_liu@live.ca>
2024-10-08 03:31:35 -0400

    filter out cats with inactive subscriptions from messaging

--- src/app.service.ts
+++ src/app.service.ts
@@ -44,7 +44,6 @@ export class AppService {
   }
 
   getOrderPrice(cats: Cat[]): number {
-    const activeSubscriptionCats = cats.filter((cat) => cat.subscriptionActive);
     const pouchSizeValues: { [key: string]: number } = {
       A: 55.5,
       B: 59.5,
@@ -54,7 +53,7 @@ export class AppService {
       F: 71.25,
     };
 
-    return activeSubscriptionCats.reduce((total, cat) => {
+    return cats.reduce((total, cat) => {
       const value = pouchSizeValues[cat.pouchSize];
       return total + (value || 0);
     }, 0);
@@ -65,11 +64,16 @@ export class AppService {
     const fileContent = readFileSync(filePath, 'utf-8');
     const myJson = JSON.parse(fileContent);
     const user = myJson.find((u: User) => u.id === userId);
-    const catNames = user.cats.map((cat: Cat) => cat.name);
+
+    const activeSubscriptionCats = user.cats.filter(
+      (cat: Cat) => cat.subscriptionActive,
+    );
+
+    // assuming that if a cat's subscription is inactive, we won't display their name at all in the messaging
+    const catNames = activeSubscriptionCats.map((cat: Cat) => cat.name);
     const catNamesList = formatCatNameList(catNames);
-    console.log(catNamesList);
-    console.log(user);
-    const orderPrice = this.getOrderPrice(user.cats);
+    const orderPrice = this.getOrderPrice(activeSubscriptionCats);
+
     return {
       title: `Your next delivery for ${catNamesList}`,
       message: `Hey ${user.firstName}! In two days' time, we'll be charging you for your next order for ${catNamesList}'s fresh food.`,
